18562fe25b78b253b6844402c5f7c649e24d5381
bangaffannn <bangaffannn@gmail.com>
2023-12-25 23:04:20 +0700

    Creating components

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Post from "../components/Post";
+
+function App() {
+  return (
+    <main>
+      <Post author="MAGM" body="React JS is awesome" />
+      <Post author="RISMA" body="Check out the full course" />
+    </main>
+)};
+
+export default App;
