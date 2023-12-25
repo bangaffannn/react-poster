68ab65793a3b00ccf7275d8cb21930609eac414a
bangaffannn <bangaffannn@gmail.com>
2023-12-26 06:17:46 +0700

    Adding useState hook

--- components/PostsList.jsx
+++ components/PostsList.jsx
@@ -1,12 +1,16 @@
 import classes from "./PostsList.module.css";
 import Post from "./Post";
+import NewPost from "./NewPost";
 
 function PostsList() {
     return (
+    <>
+        <NewPost />
         <ul className={classes.posts}>
             <Post author="MAGM" body="React JS is awesome" />
             <Post author="RISMA" body="Check out the full course" />
         </ul>
+    </>
     );
 }
 
