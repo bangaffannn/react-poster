6f9548bfd8be6310773e6d6ea4f7d4a55a070fb0
bangaffannn <bangaffannn@gmail.com>
2023-12-27 17:54:15 +0700

    Add show and hide function

--- components/PostsList.jsx
+++ components/PostsList.jsx
@@ -1,13 +1,36 @@
+import { useState } from 'react'
+
 import classes from "./PostsList.module.css";
 import Post from "./Post";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
 
-function PostsList() {
+function PostsList( {isPosting, onStopPosting} ) {
+    const [ enteredBody, setEnteredBody] = useState('');
+    const [ enteredAuthor, setEnteredAuthor] = useState('');
+
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
+
+    // APPROACH VISIBLE CONTENT USING VARIABLE
+    // let modalContent;
+    // if (modalIsVisible) {
+    //     modalContent = <Modal onClose={hideModallHandler}>
+    //         <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+    //     </Modal>
+    // }
+
     return (
     <>
-        <NewPost />
+        {isPosting && (<Modal onClose={onStopPosting}>
+            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+         </Modal>)}
         <ul className={classes.posts}>
-            <Post author="MAGM" body="React JS is awesome" />
+            <Post author={enteredAuthor} body={enteredBody} />
             <Post author="RISMA" body="Check out the full course" />
         </ul>
     </>
