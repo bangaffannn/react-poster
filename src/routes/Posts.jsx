9f6ab65114af801380dd59211ab524e9916cb575
bangaffannn <bangaffannn@gmail.com>
2023-12-28 00:55:49 +0700

    Submit data and Creating dynamic routes

--- src/routes/Posts.jsx
+++ src/routes/Posts.jsx
@@ -1,9 +1,7 @@
 import  { Outlet } from 'react-router-dom';
-import PostsList from "../../components/PostsList";
-import MainHeader from "../../components/MainHeader";
+import PostsList from '../../components/PostsList';
 
 function Posts() {
-  
   return (
     <>
       <Outlet />
@@ -15,3 +13,9 @@ function Posts() {
 }
 
 export default Posts;
+
+export async function loader() {
+  const response = await fetch('http://localhost:8080/posts')
+  const resData = await response.json();
+  return resData.posts;
+}
