3a8bdf10fb267795b79da75058f106526225ab85
bangaffannn <bangaffannn@gmail.com>
2023-12-27 19:30:31 +0700

    Handling form submission

--- components/PostsList.jsx
+++ components/PostsList.jsx
@@ -6,15 +6,6 @@ import NewPost from "./NewPost";
 import Modal from "./Modal";
 
 function PostsList( {isPosting, onStopPosting} ) {
-    const [ enteredBody, setEnteredBody] = useState('');
-    const [ enteredAuthor, setEnteredAuthor] = useState('');
-
-    function bodyChangeHandler(event) {
-        setEnteredBody(event.target.value);
-    }
-    function authorChangeHandler(event) {
-        setEnteredAuthor(event.target.value);
-    }
 
     // APPROACH VISIBLE CONTENT USING VARIABLE
     // let modalContent;
@@ -27,10 +18,10 @@ function PostsList( {isPosting, onStopPosting} ) {
     return (
     <>
         {isPosting && (<Modal onClose={onStopPosting}>
-            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+            <NewPost 
+                onCancel={onStopPosting} />
          </Modal>)}
         <ul className={classes.posts}>
-            <Post author={enteredAuthor} body={enteredBody} />
             <Post author="RISMA" body="Check out the full course" />
         </ul>
     </>
